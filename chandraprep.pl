d69c01f2df22817bbfb5e6f2e4a19d9e94e2559f
tria-git <37617248+tria-git@users.noreply.github.com>
2019-07-10 17:06:38 +0200

    Add files via upload

---
+++ chandraprep.pl
@@ -0,0 +1,65 @@
+#!/usr/bin/perl
+
+# please call ciao from command line before using the script
+
+use strict;
+use warnings;
+use diagnostics;
+use Cwd;
+
+my $ciaodownload = "download_chandra_obsid";
+my $ciaorepro = "chandra_repro";
+
+my $sourceid ="m82";
+my $datadir = "/userdata/data/graefe/chandra2";
+
+my $workingdir;
+my $ii;
+
+my @obsids=(
+'16580',
+'15616',
+'13796',
+'10545',
+'11800',
+'11104',
+'10544',
+'10925',
+'10543',
+'10542',
+'10026',
+'10025',
+'10027',
+'8190',
+'8505',
+'8189',
+'6361',
+'5644',
+'6097',
+'2933',
+'380',
+'379',
+'378',
+'1302',
+'361',
+);
+
+my $sourcedir = "$datadir/$sourceid";
+
+
+for($ii=0; $ii<@obsids; $ii++){
+    chdir($sourcedir);
+    print "downloading obsid ${obsids[$ii]}...\n";
+    system($ciaodownload,
+	   "${obsids[$ii]}"
+	);
+    my $obsdir = "$sourcedir/${obsids[$ii]}";
+    chdir($obsdir);
+    print "now working in $obsdir...\n";
+    system($ciaorepro,
+	   ".",
+	   "repro/",
+	);
+}
+
+
